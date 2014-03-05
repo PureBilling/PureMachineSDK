a8fabb6ba7b307050d7be70db12b030ae2b7b615
Lionel Martin <lm@purebilling.com>
2014-03-05 18:16:55 +0000

    HttpHelper does not check host identity ove SSL

--- src/PureMachine/Bundle/SDKBundle/Service/HttpHelper.php
+++ src/PureMachine/Bundle/SDKBundle/Service/HttpHelper.php
@@ -85,6 +85,7 @@ class HttpHelper
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_USERAGENT, 'PureMachine HttpHelpers:getJsonAnswer');
 
